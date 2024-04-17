7a6998ddff6d43d445b03ce5e204ec9f38d820bf
Thurflecks <thurflecks.dev@gmail.com>
2024-04-17 18:33:19 -0300

    vetores

---
+++ vetores/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Digite o tamanho do vetor: ");
+        Scanner sc = new Scanner(System.in);
+        int v = sc.nextInt();
+        int n[] = new int[v];
+        int m;
+
+        for (int i = 0; i < n.length; i++) {
+            System.out.println("Digite um valor: ");
+            m = sc.nextInt();
+            if (m < 0) {
+                n[i] = m;
+            }
+        }
+        for (int i : n) {
+            if(i<0){
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+
+    }
+}
